a567c9ee332e742e97754433c24e0ddecfa326cd
andrew <aqs6915@psu.edu>
2023-10-14 21:46:57 -0400

    format v0.5

---
+++ main.js
@@ -0,0 +1,42 @@
+const resources = [
+    {name: "University Fitness Center-Penn State", county: "Hershey/Hummelstown", address:"30 Conference Dr., Hershey, PA 17033", note:"	Exclusive health and wellness facility, serving our Penn State Health affiliates.Membership https://ufc.pennstatehealth.org/membership/.Staffed by exercise physiologists who can assist  with designing custom workout programs, weight training routines, improving your physical health, helping you recover from a sports injury or just providing motivation for a health, exercise and fitness goal"},
+    {name: "Movement as Medicine Dance Class", county: "Hershey/Hummelstown", address:"Contact Alexandra De La Plante", note:"delaplante@pennstatehealth.psu.edu.Free adaptive dance classes for people living with disabilities .Need to fill out attached form"},
+    {name: "East Shore Branch YMCA", county: "Harrisburg", address:"701 N Front Street, Harrisburg, PA ", note:"Full gym with ellipticals, treadmills, bikes, rowing machines, and pool access.Membership needed but will have full access to any YMCA in the area"},
+    {name: "Strava: Run, Ride, Swim", county: "Universal", address:"Download App", note:"	- Free app compatible with Apple and Samsung consumers.Tracks and logs workouts.Receive kudos from friends and family who can see your workout activity "},
+    {name: "Fitness Blender", county: "Universal", address:"https://www.fitnessblender.com/", note:"Free full-length workout videos, workout programs, meal plans, nutrition information and fitness information. Accessible to everyone with over 500+ free workouts - you just need to make an account and need access to a computer/mobile device and internet  "},
+    {name: "SilverSneakers", county: "Universal", address:"To find a participating location: https://tools.silversneakers.com/LocationSearch ", note:"Free for adults 65+ through select Medicare plans. Must check if you have SilverSneakers with your Medicare plan. Create an online account and download the membership card. Find nearest locations to join a fitness class or exercise via online classes "},
+    {name: "SmartGym: Gym & Home Workouts", county: "Universal", address:"https://smartgymapp.com/", note:"	Free app compatible with Apple consumers "},
+    {name: "Dauphin County Parks and Recreation Department ", county: "Harrisburg", address:"100 Fort Hunter Road, Harrisburg, PA 17110 ", note:"Recreational activities including jogging, festivals, concerts, historic mansion tours; environmental education; parks; maintenance and development technical assistance; recreation technical assistance "},
+    {name: "Lebanon Valley Family YMCA - Fitness and Wellness", county: "Lebanon", address:"201 North 7th Street Lebanon, PA 17046 ", note:"Facilities and programs include state-of-the-art wellness center, 3 group fitness studios including a cycling studio, group fitness classes including kids fitness classes, over 80 group fitness classes for all levels, kids corner babysitting while you exercise, water fitness classes, swim lessons, camp, sports, indoor pool, personal training, indoor track, wellness coaching, indoor basketball court, racquetball courts, two gymnasiums, healthy habits seminars, and wi-fi throughout facility. "},
+    {name: "West Shore Branch YMCA", county: "West Shore", address:"410 Fallowfield Rd, Camp Hill, PA 17011, United States", note:"Full gym with ellipticals, treadmills, bikes, rowing machines, and pool access. Membership needed but will have full access to any YMCA in the area"},
+
+    // Add more resource objects as needed
+];
+
+const buttons = document.querySelectorAll('button');
+const resourceList = document.getElementById('resource-list');
+const resourceItems = document.getElementById('resource-items');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const county = button.textContent;
+
+        // Filter resources by county
+        const countyResources = resources.filter(resource => resource.county === county);
+
+        if (countyResources.length > 0) {
+            const resourceItemsHTML = countyResources.map((resource) => {
+                return `<li>
+                    <strong>${resource.name}</strong><br>
+                    County: ${resource.county}<br>
+                    Address: ${resource.address}<br>
+                    Contact: ${resource.contact}
+                </li>`;
+            }).join('');
+            
+            resourceItems.innerHTML = resourceItemsHTML;
+            resourceList.classList.remove('hidden');
+        }
+    });
+});
+
